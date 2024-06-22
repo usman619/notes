905f317717cdb5167c903abbe7ae1836a9462f24
usman619 <usmanshahab191@gmail.com>
2024-06-22 22:21:29 +0500

    Fixing allNotes function in fireabse_cloud_storage.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ void main() {
 
   runApp(MaterialApp(
     title: 'Flutter Demo',
+    debugShowCheckedModeBanner: false,
     theme: ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       useMaterial3: true,
